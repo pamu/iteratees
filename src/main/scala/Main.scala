25d11b8422b14d56dee8f333b296bd2ff055017a
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-09-12 02:04:00 +0530

    iteratee enumerator

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,21 @@
+import play.api.libs.iteratee.{Iteratee, Enumerator}
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
 /**
  * Created by pnagarjuna on 12/09/15.
  */
 object Main {
   def main(args: Array[String]): Unit = {
-
+    val languages = Enumerator("scala", "java", "haskel", "clojure")
+    val iteratee = Iteratee.fold[String, String]("")((r, c) => s"$r => $c ")
+    val result = languages.run(iteratee)
+    result.onComplete {
+      case Success(value) => println(value)
+      case Failure(th) => println(th.getMessage)
+    }
+    Await.result(result, 10 minutes)
   }
 }
