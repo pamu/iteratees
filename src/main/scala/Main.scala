2a75d65fc3c03aa5763224dbb71c93781f0f6ceb
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-09-21 02:02:11 +0530

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import _root_.Main.{Cont, Done}
 import play.api.libs.iteratee.{Iteratee, Enumerator}
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -10,55 +11,57 @@ import scala.util.{Failure, Success}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("----------------- simple foldleft -------------")
+    println("----------------- Simple foldLeft -------------")
     val list = List(1, 2, 3, 4, 5)
-    val result = foldLeft[Int, Int](list)(0)((r, c) => r * c)
+    val result = Idea.foldLeft[Int, Int](list)(0)((r, c) => r * c)
     println(s"result $result")
 
-    println("---------------- smart foldleft --------------")
+    println("---------------- Smart foldLeft --------------")
     val strList = List("python", "ruby", "scala", "java", "c", "cpp")
-    val language = foldLeftX[List[String], String](strList)(List[String]())((r, c) =>
+    val language = Smart.foldLeft[List[String], String](strList)(List[String]())((r, c) =>
       if (c == "scala") {
-        Done(List[String]("scala is my favourite language"))
+        Smart.Done(List[String]("scala is my favourite language"))
       } else {
         println("This is not favourite language")
-        Cont(r)
+        Smart.Cont(r)
       }
     )
     language.foreach(println)
 
+    println("---------------- Super Smart foldLeft --------------")
+
 
   }
 
-  def foldLeft[B, A](iStream: List[A])(acc: B)(f: (B, A) => B): B = {
-    def loop(acc: B)(list: List[A]): B = list match {
-      case Nil => acc
-      case x :: Nil => loop(f(acc, x))(Nil)
-      case x :: xs => loop(f(acc, x))(xs)
+  object Idea {
+    def foldLeft[B, A](iStream: List[A])(acc: B)(f: (B, A) => B): B = {
+      def loop(acc: B)(list: List[A]): B = list match {
+        case Nil => acc
+        case x :: Nil => loop(f(acc, x))(Nil)
+        case x :: xs => loop(f(acc, x))(xs)
+      }
+      loop(acc)(iStream)
     }
-    loop(acc)(iStream)
   }
 
-
-  sealed trait Step[+B]
-
-  case class Done[+B](result: B) extends Step[B]
-
-  case class Cont[+A](value: A) extends Step[A]
-
-  def foldLeftX[B, A](iStream: List[A])(acc: B)(f: (B, A) => Step[B]): B = {
-    def loop(list: List[A])(acc: B): B = list match {
-      case Nil => acc
-      case x :: Nil => f(acc, x) match {
-        case Done(result) => result
-        case Cont(value) => loop(Nil)(value)
+  object Smart {
+    sealed trait Step[+B]
+    case class Done[+B](result: B) extends Step[B]
+    case class Cont[+A](value: A) extends Step[A]
+    def foldLeft[B, A](iStream: List[A])(acc: B)(f: (B, A) => Step[B]): B = {
+      def loop(list: List[A])(acc: B): B = list match {
+        case Nil => acc
+        case x :: Nil => f(acc, x) match {
+          case Done(result) => result
+          case Cont(value) => loop(Nil)(value)
+        }
+        case x :: xs => f(acc, x) match {
+          case Done(result) => result
+          case Cont(value) => loop(xs)(value)
+        }
       }
-      case x :: xs => f(acc, x) match {
-        case Done(result) => result
-        case Cont(value) => loop(xs)(value)
-      }
+      loop(iStream)(acc)
     }
-    loop(iStream)(acc)
   }
 
 
