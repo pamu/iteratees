c930b4e5843742bcab420d61a522f88364b2d971
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-09-21 00:38:44 +0530

    iteratees tutorial

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,18 +8,21 @@ import scala.util.{Failure, Success}
  * Created by pnagarjuna on 12/09/15.
  */
 object Main {
+
   def main(args: Array[String]): Unit = {
-    val languages = Enumerator("scala", "java", "haskel", "clojure")
-    val iteratee = Iteratee.fold[String, String]("")((r, c) => s"$r => $c ")
-    val result = languages |>> iteratee
-    val res1 = Iteratee.flatten(result).run
-    res1.onComplete {
-      case Success(value) => println(value)
-      case Failure(th) => println(th.getMessage)
-    }
-
-
-
-    Await.result(result, 10 minutes)
+    val list = List(1, 2, 3, 4, 5)
+    val result = foldLeft[Int, Int](list)(0)((r, c) => r * c)
+    println(s"result $result")
   }
+
+  def foldLeft[B, A](iStream: List[A])(acc: B)(f: (B, A) => B): B = {
+    def loop(acc: B)(list: List[A]): B = list match {
+      case Nil => acc
+      case x :: Nil => loop(f(acc, x))(Nil)
+      case x :: xs => loop(f(acc, x))(xs)
+    }
+    loop(acc)(iStream)
+  }
+
+
 }
