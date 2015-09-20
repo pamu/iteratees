5960e5d9deefdf4812b83af051768c6bf686dd71
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-09-21 01:52:54 +0530

    smart foldleft demo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,9 +10,24 @@ import scala.util.{Failure, Success}
 object Main {
 
   def main(args: Array[String]): Unit = {
+    println("----------------- simple foldleft -------------")
     val list = List(1, 2, 3, 4, 5)
     val result = foldLeft[Int, Int](list)(0)((r, c) => r * c)
     println(s"result $result")
+
+    println("---------------- smart foldleft --------------")
+    val strList = List("python", "ruby", "scala", "java", "c", "cpp")
+    val language = foldLeftX[List[String], String](strList)(List[String]())((r, c) =>
+      if (c == "scala") {
+        Done(List[String]("scala is my favourite language"))
+      } else {
+        println("This is not favourite language")
+        Cont(r)
+      }
+    )
+    language.foreach(println)
+
+
   }
 
   def foldLeft[B, A](iStream: List[A])(acc: B)(f: (B, A) => B): B = {
@@ -38,8 +53,14 @@ object Main {
         case Done(result) => result
         case Cont(value) => loop(Nil)(value)
       }
+      case x :: xs => f(acc, x) match {
+        case Done(result) => result
+        case Cont(value) => loop(xs)(value)
+      }
     }
     loop(iStream)(acc)
   }
+
+
 
 }
